8961c6a7cc706bb8603259b7ae89c0badf2b512d
SarathVaragani <126099786+SarathVaragani@users.noreply.github.com>
2024-02-15 10:00:02 +0000

    removing unwanted files

--- Framework/src/pageobjects/MyPageObject.java
+++ Framework/src/pageobjects/MyPageObject.java
@@ -13,7 +13,5 @@ import com.provar.core.testapi.annotations.*;
      , connection="YHTest_Admin"
      )             
 public class MyPageObject {
-
-	@TextType() @FindBy(xpath="//div[contains(@class,'active') and contains(@class,'oneWorkspace') and (ancestor::div[contains(@class,'active') and contains(@class,'main-content')]//div[contains(@class, 'oneGlobalNav') or contains(@class, 'tabBarContainer')]//div[contains(@class, 'tabContainer') and contains(@class, 'active')] )]//h2[normalize-space(.)='New Case']") public WebElement _;
 			
 }
